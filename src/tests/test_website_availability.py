70d770ca89a51b0ba805ffa5bec52c3039535cd3
Ubuntu <ubuntu@ip-172-31-91-159.ec2.internal>
2020-09-25 01:12:46 +0000

    Added deployment job

--- src/tests/test_website_availability.py
+++ src/tests/test_website_availability.py
@@ -5,7 +5,7 @@ from src.website_availability import WebsiteAvailability
 class TestWebsiteAvailabilityFunctions(unittest.TestCase):
     
     def test_get_ip_address(self):
-        website = WebsiteAvailability("sydneyhearingclinic.com.au") # os.environ.get("TEST_DOMAIN")
+        website = WebsiteAvailability("nerdypandy.com.au") # os.environ.get("TEST_DOMAIN")
         ip_address = website.get_ip_address()
         
         self.assertIsInstance(ip_address, str)
@@ -13,13 +13,13 @@ class TestWebsiteAvailabilityFunctions(unittest.TestCase):
         self.assertEqual(ip_address, "149.28.188.138" ) # os.environ.get("TEST_IP_ADDRESS")
         
     def test_get_http_status_code(self):
-        website = WebsiteAvailability("sydneyhearingclinic.com.au")
+        website = WebsiteAvailability("nerdypandy.com.au")
         http_status_code = website.get_http_status_code()
         
         self.assertIsInstance(http_status_code, tuple)
         
     def test_check_whois_status(self):
-        website = WebsiteAvailability("sydneyhearingclinic.com.au")
+        website = WebsiteAvailability("nerdypandy.com.au")
         server_and_content_type = website.get_server_and_content_type()
         
         self.assertIsInstance(server_and_content_type, tuple)
