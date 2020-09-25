53d7a01d1aae0bddee9c23344fc2cc17f33016cb
Ubuntu <ubuntu@ip-172-31-91-159.ec2.internal>
2020-09-25 00:44:03 +0000

    Test

--- src/tests/test_scrape_website.py
+++ src/tests/test_scrape_website.py
@@ -5,6 +5,6 @@ from src.scrape_website import ScrapeWebsite
 
 class TestScrapeWebsiteFunctions(unittest.TestCase):
     def test_get_site_name(self):
-        website = ScrapeWebsite(os.environ.get("TEST_DOMAIN"))
+        website = ScrapeWebsite("sydneyhearingclinic.com.au")
         result = website.get_site_name()
         self.assertIsInstance(result, str)
